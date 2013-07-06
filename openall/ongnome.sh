7ae8fd931e1dd13aafbc3489bc0d60ee917ec24a
scottmotte <scott@scottmotte.com>
2013-07-06 15:34:06 +0000

    Conditional statement for openalls

--- openall/ongnome.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-ack -o --perl '(?<=\()(http.*?)(?=\))' README.md | while read LINE; do gnome-open $LINE; done
--- openall/onkde.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-ack -o --perl '(?<=\()(http.*?)(?=\))' README.md | while read LINE; do kde-open $LINE; done
